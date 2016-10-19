cfaf05d51e132bff6b0f3a03a90d310695ff3907
Philip Clifton <philip.clifton@flightaware.com>
2016-10-19 13:06:56 -0500

    Revise Gruntfile for FA release tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,33 @@
 "use strict";
 
 module.exports = function(grunt) {
+	/**
+	 * We build a set of files, each consisting of the moment-timezone library and
+	 * two years worth of TZ data. This allows us to send down the minimum TZ data
+	 * required for a given page. We do pairs instead of individual years so that
+	 * a flight happening on New Year's Eve can still work correctly.
+	 */
+	
+	// Build using data from 2004 to four years in the future
+	var range = [2004, new Date().getFullYear() + 4];
+	
+	// Defines the build for unminified src JS
+	var buildObj = {};
+	// Defines the minification task (via uglify)
+	var fileObj = {};
+	
+	// Make the config objects
+	for (var start = range[0]; start < range[1]; start++) {
+		var end = start + 1;
+		var thisRange = [start, end];
+		var thisName = thisRange.join('-');
+		var filename = 'builds/' + thisName + '.js';
+		var minFilename = 'builds/' + thisName + '.min.js';
+		
+		buildObj[thisName] = thisRange;
+		fileObj[minFilename] = filename;
+	}
+	
 	grunt.initConfig({
 		nodeunit : {
 			zones : [
@@ -11,18 +38,11 @@ module.exports = function(grunt) {
 			]
 		},
 
-		build : {
-			'moment-timezone-with-data'           : true,
-			'moment-timezone-with-data-2010-2020' : [2010, 2020]
-		},
+		build : buildObj,
 
 		uglify : {
 			all: {
-				files: {
-					'builds/moment-timezone.min.js'                     : 'moment-timezone.js',
-					'builds/moment-timezone-with-data.min.js'           : 'builds/moment-timezone-with-data.js',
-					'builds/moment-timezone-with-data-2010-2020.min.js' : 'builds/moment-timezone-with-data-2010-2020.js'
-				}
+				files: fileObj
 			},
 			options: {
 				report : 'gzip',
@@ -35,7 +55,8 @@ module.exports = function(grunt) {
 		},
 
 		clean: {
-			data: ['temp']
+			data: ['temp'],
+			unmin: ['builds/*', '!builds/*.min.js']
 		}
 	});
 
@@ -46,7 +67,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 
-	grunt.registerTask('release', ['jshint', 'data', 'nodeunit', 'build', 'uglify']);
+	grunt.registerTask('release', ['jshint', 'data', 'nodeunit', 'build', 'uglify', 'clean']);
 
 	grunt.registerTask('default', ['jshint', 'nodeunit']);
 };
